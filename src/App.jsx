b0de8b0ffb0b5600b533c60d5ea03618156dc7a0
meecan <cannhann42@gmail.com>
2023-08-12 00:21:35 +0300

    Header component created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "~/components/Header"
+
 function App() {
   return (
-    <div>
-      home
-    </div>
+    <>
+      <Header></Header>
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="border-b border-[#23262f] h-20">
+      <div className="container h-full flex items-center">
+        Header
+      </div>
+    </div>
+  )
+}
+
+export default Header
