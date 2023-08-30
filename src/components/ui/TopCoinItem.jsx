a8f4fc91458ff0f0ddffdcafd6d24d78898c9b14
Mehmet Can Erturk <cannhann42@gmail.com>
2023-08-30 23:17:37 +0300

    Added styles of TopCoinItem component.

--- src/components/ui/TopCoinItem.jsx
+++ src/components/ui/TopCoinItem.jsx
@@ -1,7 +1,12 @@
-function TopCoinItem() {
+function TopCoinItem({ name, image, price, change }) {
   return (
-    <div className="col-span-3 bg-red-500">
-      Top Coin
+    <div className="col-span-3 flex flex-col items-center">
+      <img src={image} className="w-24" />
+        <div className="font-semibold text-2xl mt-4">{name}</div>
+        <div className="flex gap-2 mt-2">
+          <span className="font-normal text-lg">${price}</span>
+          <span className="font-normal text-lg">{change}%</span>
+        </div>
     </div>
   )
 }
