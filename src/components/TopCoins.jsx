f104dcdcfb670704f3ea15c04b20119fe589072e
Mehmet Can Erturk <cannhann42@gmail.com>
2023-08-30 23:28:54 +0300

    Listed first 4 values of the prices from the global state.

--- src/components/TopCoins.jsx
+++ src/components/TopCoins.jsx
@@ -1,12 +1,22 @@
+import { useSelector } from "react-redux"
 import TopCoinItem from "~/components/ui/TopCoinItem"
 
 function TopCoins() {
+
+  const prices = useSelector((state) => state.crypto.prices)
+  const topCoins = prices.slice(0, 4)
+  
   return (
-    <div className="container grid grid-cols-12 gap-6 py-8 max-sm:grid-cols-6 max-[340px]:grid-cols-3">
-      <TopCoinItem></TopCoinItem>
-      <TopCoinItem></TopCoinItem>
-      <TopCoinItem></TopCoinItem>
-      <TopCoinItem></TopCoinItem>
+    <div className="container grid grid-cols-12 gap-6 py-14 max-sm:grid-cols-6 max-[340px]:grid-cols-3">
+      {topCoins.map((coin, index) => (
+        <TopCoinItem 
+          key={index}
+          name={coin.name}
+          image={coin.image}
+          price={coin.current_price}
+          change={coin.price_change_percentage_24h}
+        />
+      ))}
     </div>
   )
 }
