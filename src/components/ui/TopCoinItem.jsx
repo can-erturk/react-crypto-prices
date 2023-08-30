61b00fb738fbebd451cae0d06505973352e71ee4
Mehmet Can Erturk <cannhann42@gmail.com>
2023-08-30 23:22:44 +0300

    Price change percentage limited to 3 decimal digits.

--- src/components/ui/TopCoinItem.jsx
+++ src/components/ui/TopCoinItem.jsx
@@ -1,11 +1,20 @@
 function TopCoinItem({ name, image, price, change }) {
+
+  let slicedChange = change.toString()
+
+  if (slicedChange.startsWith('-')) {
+    slicedChange = slicedChange.substr(0, 5)
+  } else {
+    slicedChange = slicedChange.substr(0, 4)
+  }
+
   return (
     <div className="col-span-3 flex flex-col items-center">
       <img src={image} className="w-24" />
         <div className="font-semibold text-2xl mt-4">{name}</div>
         <div className="flex gap-2 mt-2">
           <span className="font-normal text-lg">${price}</span>
-          <span className="font-normal text-lg">{change}%</span>
+          <span className="font-normal text-lg">{slicedChange}%</span>
         </div>
     </div>
   )
