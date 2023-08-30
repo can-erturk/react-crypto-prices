51b50ae552783b055ada249d299214d88eff4e65
Mehmet Can Erturk <cannhann42@gmail.com>
2023-08-30 21:52:34 +0300

    Prices fetched from API and added to the global state.

--- src/components/MarketUpdate.jsx
+++ src/components/MarketUpdate.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { setPrices } from "~/stores/prices"
+import getPrices from "~/services/getPrices"
+
 function MarketUpdate() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const prices = await getPrices("/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100")
+      dispatch(setPrices(prices))
+    }
+
+    fetchData()
+  }, [dispatch])
+
   return (
     <>Market Update</>
   )
