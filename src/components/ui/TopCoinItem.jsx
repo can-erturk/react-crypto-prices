69b421e7f42f074af57a2857cd67079cd8497f7c
Mehmet Can Erturk <cannhann42@gmail.com>
2023-08-30 23:24:40 +0300

    Price change highlighted with color.

--- src/components/ui/TopCoinItem.jsx
+++ src/components/ui/TopCoinItem.jsx
@@ -1,11 +1,13 @@
 function TopCoinItem({ name, image, price, change }) {
 
   let slicedChange = change.toString()
+  let changeColor = "text-red-500"
 
   if (slicedChange.startsWith('-')) {
     slicedChange = slicedChange.substr(0, 5)
   } else {
     slicedChange = slicedChange.substr(0, 4)
+    changeColor = "text-green-500"
   }
 
   return (
@@ -14,7 +16,7 @@ function TopCoinItem({ name, image, price, change }) {
         <div className="font-semibold text-2xl mt-4">{name}</div>
         <div className="flex gap-2 mt-2">
           <span className="font-normal text-lg">${price}</span>
-          <span className="font-normal text-lg">{slicedChange}%</span>
+          <span className={"font-normal text-lg " + changeColor}>{slicedChange}%</span>
         </div>
     </div>
   )
