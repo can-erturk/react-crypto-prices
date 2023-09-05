ec1308747d322b9ec4e9dfa21c60ac2ea80b6998
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-05 22:43:13 +0300

    Added state and reducer for table pagination.

--- src/stores/table.jsx
+++ src/stores/table.jsx
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
   currentPage: 1,
+  paginatedData: [],
 }
 
 const table = createSlice({
@@ -11,8 +12,12 @@ const table = createSlice({
     setCurrentPage: (state, action) => {
       state.currentPage = action.payload
     },
+    setPaginatedData: (state, action) => {
+      state.paginatedData = action.payload
+    },
   },
 })
 
 export const { setCurrentPage } = table.actions
+export const { setPaginatedData } = table.actions
 export default table.reducer
