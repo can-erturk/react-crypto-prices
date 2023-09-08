61c83a4734aea21597383cc37ec31bc3dd7dd91a
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-08 16:33:02 +0300

    Sorting icons updated.

--- src/components/DataTable/ui/SortingIcon.jsx
+++ src/components/DataTable/ui/SortingIcon.jsx
@@ -1,4 +1,4 @@
-import { FaSortAmountDown, FaSortAmountUp } from "react-icons/fa"
+import { BiSort } from "react-icons/bi"
 import { useSelector } from "react-redux"
 
 function SortingIcon({ id }) {
@@ -6,7 +6,21 @@ function SortingIcon({ id }) {
   const sortConfig = useSelector(state => state.table.sortConfig)
 
   if (sortConfig.key === id) {
-    return sortConfig.direction === "ascending" ? <FaSortAmountUp /> : <FaSortAmountDown />
+    if (sortConfig.direction === "ascending") {
+      return (
+        <span className="ascending-sort-icon">
+          <BiSort size={20} />
+        </span>
+      )
+    } else {
+      return (
+        <span className="descending-sort-icon">
+          <BiSort size={20} />
+        </span>
+      )
+    }
+  } else {
+    return <BiSort className="opacity-30" size={20} />
   }
 }
 
