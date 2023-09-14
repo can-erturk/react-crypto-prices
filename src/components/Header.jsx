aa227984e9884afdbd3b886ef195087a848f6df9
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-14 11:41:11 +0300

    Header styles updated.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   }
 
   return (
-    <div className="bg-transparent absolute w-full h-20">
+    <div className="bg-transparent absolute w-full h-28">
       <div className="container h-full flex items-center justify-between">
         
         {/* Logo */}
@@ -17,18 +17,19 @@ function Header() {
         </div>
         
         {/* Navbar (Mobile sidebar) */}
-        <nav className="navbar mobile-sidebar">
+        <nav className="navbar mobile-sidebar h-full">
           <span className="sidebar-close" onClick={sidebarToggle}>
             <FaXmark size={20} />
           </span>
-          <a href="#" className="p-2">Home</a>
-          <a href="#" className="p-2">Popular</a>
-          <a href="#" className="p-2">Coin List</a>
-          <a href="#" className="navbar-btn">Join Us</a>
+          <a href="#" className="p-2 uppercase">Home</a>
+          <a href="#" className="p-2 uppercase">Popular</a>
+          <a href="#" className="p-2 uppercase">Coin List</a>
         </nav>
         
         {/* Mobile sidebar backdrop */}
         <div className="sidebar-backdrop" onClick={sidebarToggle}></div>
+        
+        <a href="#" className="navbar-btn">Get In Touch</a>
         
         {/* Mobile sidebar toggle */}
         <span className="lg:hidden px-3 py-4" onClick={sidebarToggle}>
