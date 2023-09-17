4e2614b81a1f3bc9e5e75dedf8139835468c9b56
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-17 17:23:59 +0300

    Unnecessary layout store removed.

--- src/stores/index.jsx
+++ src/stores/index.jsx
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import priceReducer from "./prices"
 import tableReducer from "./table"
-import layoutReducer from "./layout"
 
 const store = configureStore({
   reducer: {
     crypto: priceReducer,
-    table: tableReducer,
-    layout: layoutReducer
+    table: tableReducer
   },
 })
 
