58e1b047512fd2df1337ecf0d41747ca36ebf263
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-20 18:38:04 +0300

    Hero styles updated.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,15 @@
 function Hero() {
   return (
-    <div className="h-[700px] max-lg:h-[400px] max-sm:h-[250px] pt-28 max-sm:pt-20">
+    <div className="h-[700px] max-lg:h-[550px] max-sm:h-[90dvh] pt-28 max-sm:pt-20 pb-20 max-lg:pb-16 max-sm:pb-10 border-b border-default">
       <div className="container h-full relative flex flex-wrap items-center justify-between overflow-hidden">
-        <div className="sm:w-2/3 text-center m-auto">
-          <h1 className="text-7xl flex-grow font-medium max-lg:text-4xl max-sm:text-xl">
+        <div className="lg:w-2/3 text-center m-auto">
+          <h1 className="text-7xl flex-grow font-medium max-lg:text-5xl max-sm:text-4xl">
             Today&apos;s Cryptocurrency Prices
           </h1>
           <span className="text-gray-400 text-2xl block mt-3 max-lg:text-lg max-sm:text-sm">
             The global market cap is <b className="font-semibold">$1.02T</b>
           </span>
+          <a href="#" className="hero-btn">Take a Look</a>
         </div>
       </div>
     </div>
