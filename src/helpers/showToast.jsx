a6866aff8702f59d2370873b849a424ad9c41aef
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-24 00:46:43 +0300

    'apiError' toast message styles updated.

--- src/helpers/showToast.jsx
+++ src/helpers/showToast.jsx
@@ -2,11 +2,15 @@ import { toast } from "react-toastify"
 
 const demoAlert = () => toast.info("This is a demo feature and is currently unavailable.")
 
-const apiError = () => {
-  toast.error("Outdated data is being displayed due to an invalid response from the API. The cause of this error could be too many requests. Error code: 429", {
-    autoClose: 5000
-  })
-}
+const apiError = () => toast.error(
+  <span>
+    <span className="text-sm">Outdated data is being displayed due to an invalid response from the API.</span>
+    <div className="text-xs">
+      <span>Error code: 429</span>
+      <button className="toast-btn">Dont show again</button>
+    </div>
+  </span>
+)
 
 const showToast = {
   demoAlert,
