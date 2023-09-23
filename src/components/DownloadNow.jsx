3b9544632b65e904e0374ec78b404e7c2d19568a
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-23 21:27:43 +0300

    Notifications added for demo features in DownloadNow section.

--- src/components/DownloadNow.jsx
+++ src/components/DownloadNow.jsx
@@ -1,6 +1,11 @@
+import showToast from "~/helpers/showToast"
 import SectionTitle from "./ui/SectionTitle"
 
 function DownloadNow() {
+
+  // Function for handling demo alerts when buttons are clicked
+  const demoAlertHandler = () => showToast.demoAlert()
+
   return (
     <section id="download-now">
       <div className="container">
@@ -11,8 +16,8 @@ function DownloadNow() {
         />
 
         <div className="flex max-sm:flex-col gap-4 justify-center items-center -mt-2 max-sm:-mt-6">
-          <button className="bg-brand py-3 px-6 rounded-full font-medium">Start Subscription</button>
-          <button className="font-medium py-3 px-6">Try for free</button>
+          <button className="bg-brand py-3 px-6 rounded-full font-medium" onClick={demoAlertHandler}>Start Subscription</button>
+          <button className="font-medium py-3 px-6" onClick={demoAlertHandler}>Try for free</button>
         </div>
 
       </div>
