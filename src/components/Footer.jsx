9fe4e25472caa61663ec2e9c24b13be40b726eb0
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-24 01:04:58 +0300

    Notifications added for demo features in Footer section.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,23 @@
 import { FaDiscord, FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa6"
+import showToast from "~/helpers/showToast"
 
 function Footer() {
+
+  // Function for handling demo alerts when buttons are clicked
+  const demoAlertHandler = () => showToast.demoAlert()
+
   return (
     <div className="bg-secondary">
       <div className="container pt-12 pb-12 flex flex-col items-center gap-2">
         <div className="flex items-center gap-2">
-          <a href="#" className="inline-block p-2 transition-all hover:text-[#179cf0]"><FaTwitter size={30} /></a>
-          <a href="#" className="inline-block p-2 transition-all hover:text-[#5865f2]"><FaDiscord size={34} /></a>
-          <a href="#" className="inline-block p-2 transition-all hover:text-[#1f7bf2]"><FaFacebook size={30} /></a>
-          <a href="#" className="inline-block p-2 transition-all hover:text-[#ff0808]"><FaYoutube size={34} /></a>
+          <a className="inline-block p-2 transition-all hover:text-[#179cf0] cursor-pointer" onClick={demoAlertHandler}><FaTwitter size={30} /></a>
+          <a className="inline-block p-2 transition-all hover:text-[#5865f2] cursor-pointer" onClick={demoAlertHandler}><FaDiscord size={34} /></a>
+          <a className="inline-block p-2 transition-all hover:text-[#1f7bf2] cursor-pointer" onClick={demoAlertHandler}><FaFacebook size={30} /></a>
+          <a className="inline-block p-2 transition-all hover:text-[#ff0808] cursor-pointer" onClick={demoAlertHandler}><FaYoutube size={34} /></a>
         </div>
         <div className="flex gap-2">
-          <a href="#" className="inline-block p-2 opacity-60 hover:underline">Terms of Use</a>
-          <a href="#" className="inline-block p-2 opacity-60 hover:underline">Privacy Policy</a>
+          <a className="inline-block p-2 opacity-60 hover:underline cursor-pointer" onClick={demoAlertHandler}>Terms of Use</a>
+          <a className="inline-block p-2 opacity-60 hover:underline cursor-pointer" onClick={demoAlertHandler}>Privacy Policy</a>
         </div>
       </div>
     </div>
