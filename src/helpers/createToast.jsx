fb15823d556935bb428dd69511c18cd9835a5a75
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-23 21:14:10 +0300

    Toast messages manage method updated.

--- src/helpers/createToast.jsx
+++ src/helpers/createToast.jsx
@@ -1,24 +1,16 @@
 import { toast } from "react-toastify"
 
-// Define available toast messages
-const toastMessages = {
-  demoAlert: {
-    type: "info",
-    message: "This is a demo feature and is currently unavailable.",
-  },
-  apiError: {
-    type: "error",
-    message: "Outdated data is being displayed due to an invalid response from the API. The cause of this error could be too many requests. Error code: 429",
-  },
+const demoAlert = () => toast.info("This is a demo feature and is currently unavailable.")
+
+const apiError = () => {
+  toast.error("Outdated data is being displayed due to an invalid response from the API. The cause of this error could be too many requests. Error code: 429", {
+    autoClose: 5000
+  })
 }
 
-const createToast = (key, autoClose = 5000) => {
-
-  // Extract type and message from specified toast message
-  const { type, message } = toastMessages[key]
-  
-  // Display toast message with its type and autoClose duration
-  toast[type](message, { autoClose })
+const showToast = {
+  demoAlert,
+  apiError
 }
 
-export default createToast
+export default showToast
