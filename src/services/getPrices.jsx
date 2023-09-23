b2b224b912e22153750519df313405a8dc860b09
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-24 00:33:08 +0300

    API helper function updated to include error handling.

--- src/services/getPrices.jsx
+++ src/services/getPrices.jsx
@@ -1,14 +1,48 @@
 import axios from "axios"
+import showToast from "~/helpers/showToast"
 
 const baseURL = "https://api.coingecko.com/api/v3"
 const api = axios.create({ baseURL })
 
-async function getPrices(endPoint) {
+// A variable used to show error message only once
+let apiErrorShown = false
+
+// Error handling function
+function handleError(error) {
+  if (!apiErrorShown) {
+    showToast.apiError()
+    apiErrorShown = true
+  }
+  throw error
+}
+
+// First request to API
+async function firstApiRequest(endPoint) {
   try {
     const response = await api.get(endPoint)
     return response.data
   } catch (error) {
-    throw error
+    return handleError(error)
+  }
+}
+
+// Second request to Mock data
+async function secondApiRequest() {
+  try {
+    const response = await axios.get('/data.json')
+    return response.data
+  } catch (error) {
+    return handleError(error)
+  }
+}
+
+// Main function that manages API requests
+async function getPrices(endPoint) {
+  try {
+    return await firstApiRequest(endPoint)
+  } catch (error) {
+    // If the first API request failed
+    return await secondApiRequest()
   }
 }
 
