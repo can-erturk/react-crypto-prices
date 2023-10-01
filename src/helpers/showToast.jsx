bb3f63389170ccb6a72ead8bab773d55c00e1c85
Mehmet Can Erturk <cannhann42@gmail.com>
2023-10-01 23:19:05 +0300

    Updated toast messages and their usages.

--- src/helpers/showToast.jsx
+++ src/helpers/showToast.jsx
@@ -1,20 +1,54 @@
 import { toast } from "react-toastify"
 
-const demoAlert = () => toast.info("This is a demo feature and is currently unavailable.")
+/*
+  Variables used to show error messages only once
+  <React.StrictMode> causes the toast to be shown twice
+*/
+let axiosErrorShown = false
+let apiErrorShown = JSON.parse(localStorage.getItem("apiErrorShown")) || false
 
-const apiError = () => toast.error(
-  <span>
-    <span className="text-sm">Outdated data is being displayed due to an invalid response from the API.</span>
-    <div className="text-xs">
-      <span>Error code: 429</span>
-      <button className="toast-btn" onClick={() => localStorage.setItem("apiErrorShown", true)}>Dont show again</button>
-    </div>
-  </span>
-)
+// Toast message for demo features
+const demoAlert = () => {
+  toast.info("This is a demo feature and is currently unavailable.")
+}
 
+// Toast message for Axios errors
+const axiosError = () => {
+
+  // Show error message only once
+  if (axiosErrorShown) return
+  axiosErrorShown = true
+
+  // Showing toast
+  toast.error("An unknown error occurred and the data couldn't be fetched. Please check the browser console for more.", {
+    autoClose: false
+  })
+}
+
+// Toast message for API errors
+const apiError = () => {
+
+  // Show error message only once
+  if (apiErrorShown) return
+  apiErrorShown = true
+
+  // Showing toast
+  toast.error(
+    <>
+      <span className="text-sm">Outdated data is being displayed due to an invalid response from the API.</span>
+      <div className="text-xs">
+        <span>Error code: 429</span>
+        <button className="toast-btn" onClick={() => localStorage.setItem("apiErrorShown", true)}>Dont show again</button>
+      </div>
+    </>
+  )
+}
+
+// Export all toast messages as a single object
 const showToast = {
   demoAlert,
-  apiError
+  axiosError,
+  apiError,
 }
 
 export default showToast
