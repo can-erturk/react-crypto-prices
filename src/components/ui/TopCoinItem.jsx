cee7759f6c304944258b10c86a17168ee8e7b34d
Can Erturk <cannhann42@gmail.com>
2023-12-14 23:11:23 +0300

    Update TopCoinItem component styles

--- src/components/ui/TopCoinItem.jsx
+++ src/components/ui/TopCoinItem.jsx
@@ -7,13 +7,13 @@ function TopCoinItem({ name, image, price, change }) {
   const changeColor = changeClass(change)
 
   return (
-    <div className="bg-primary col-span-4 border border-default rounded-3xl p-8 pr-2 flex items-center gap-6 overflow-hidden">
-      <img src={image} className="w-12 h-12" />
+    <div className="bg-primary col-span-4 border border-default rounded-3xl p-6 pr-2 flex items-center gap-6 overflow-hidden">
+      <img src={image} className="w-10 h-10" />
       <div className="flex flex-col">
-        <div className="font-semibold text-2xl max-sm:text-lg line-clamp-1">
+        <div className="font-normal text-xl max-sm:text-lg line-clamp-1">
           {name}
         </div>
-        <div className="flex flex-wrap gap-2 mt-2">
+        <div className="flex flex-wrap gap-2 mt-1">
           <span>${price}</span>
           <span className={changeColor}>{slicedChange}%</span>
         </div>
