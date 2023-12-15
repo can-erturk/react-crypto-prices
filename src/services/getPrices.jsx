1fed2e24174dcbeee3e4126dd1d0a250eb5b4322
Can Erturk <cannhann42@gmail.com>
2023-12-16 01:39:30 +0300

    Update API endpoint in getPrices.jsx - If there is an API key in the .env file, use it in the endpoint - If there is no API key in the .env file, use the default endpoint

--- src/services/getPrices.jsx
+++ src/services/getPrices.jsx
@@ -3,6 +3,10 @@ import showToast from '~/helpers/showToast'
 
 const baseURL = 'https://api.coingecko.com/api/v3'
 const api = axios.create({ baseURL })
+const apiKey = import.meta.env.VITE_APP_API_KEY
+const endpoint = apiKey
+  ? `/coins/markets?x_cg_demo_api_key=${apiKey}&vs_currency=usd&order=market_cap_desc&per_page=100`
+  : '/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100'
 
 async function getMockPrices() {
   try {
@@ -16,9 +20,7 @@ async function getMockPrices() {
 
 async function getPrices() {
   try {
-    const response = await api.get(
-      '/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100',
-    )
+    const response = await api.get(endpoint)
     return response.data
   } catch (error) {
     showToast.apiError()
